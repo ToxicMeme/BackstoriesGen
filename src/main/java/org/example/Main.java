84b4d3e4a664635b099cc95e87582ae53766eae5
ToxicMeme <ivan.isaev67@yandex.ru>
2025-01-09 16:30:07 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Character hero = new Character();
+        System.out.println(hero.initializeCharacter());
+        System.out.println(hero.generatedStory());
+    }
+}
